8d9336b1b2fcd3f20ea276fba5207e306663f2a3
Rithy M <webmasterdragon@outlook.com>
2018-07-05 17:46:54 +0700

    spark v6.0 install

--- config/app.php
+++ config/app.php
@@ -154,6 +154,9 @@ return [
         /*
          * Application Service Providers...
          */
+        Laravel\Spark\Providers\SparkServiceProvider::class,
+        App\Providers\SparkServiceProvider::class,
+        Laravel\Cashier\CashierServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
